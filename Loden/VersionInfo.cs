7e850b86e452616bde174199acb256a184c7d9e0
Robert Adams <misterblue@misterblue.com>
2024-01-30 18:33:16 +0000

    Merge branch dotnet6 into master. Bump version number.

--- Loden/VersionInfo.cs
+++ Loden/VersionInfo.cs
@@ -3,8 +3,8 @@
 using System;
 namespace org.herbal3d.Loden {
     public class VersionInfo {
-        public static string appVersion = "2.2.4";
-        public static string longVersion = "2.2.4-20230720-56382a6d";
-        public static string buildDate = "20230720";
+        public static string appVersion = "2.2.5";
+        public static string longVersion = "2.2.5-20240130-837fa4b1";
+        public static string buildDate = "20240130";
     }
 }
